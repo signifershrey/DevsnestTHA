c75617744c8db325cf5a61d183820c15d1cb443f
Shrey Maurya <shreymaurya2000@gmail.com>
2021-06-14 20:22:16 +0530

    Day7 Completed✔

---
+++ Day7/q3.js
@@ -0,0 +1,15 @@
+// Write a JavaScript program to get the length of a JavaScript object.
+// Sample object: var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+
+var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
+
+
+var  arr = Object.keys(student)
+
+
+console.log(arr.length);
