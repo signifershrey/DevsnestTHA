07c99c379715104bc7c54ee0c5528b51107eb13b
Shrey Maurya <shreymaurya2000@gmail.com>
2021-07-09 02:47:54 +0530

    day16part1 done

--- Day16/first/src/App.js
+++ Day16/first/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="meme-card">
+        <img
+          src="https://www.lboro.ac.uk/media/wwwlboroacuk/external/content/mediacentre/pressreleases/2018/10/maxresdefault.jpg"
+          alt="meme-image"
+        />
+        <h1>React Card</h1>
+        <p>This a meme card</p>
+      </div>
     </div>
   );
 }
