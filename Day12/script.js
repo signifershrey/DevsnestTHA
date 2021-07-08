f4ef046e2c5031eebfd8262139c0744475791d63
Shrey Maurya <shreymaurya2000@gmail.com>
2021-07-08 22:33:22 +0530

    day-12 completed

--- Day12/script.js
+++ Day12/script.js
@@ -1,60 +1,123 @@
-var addButton = document.getElementById("add");
-addButton.addEventListener("click", addItem);
+//on refreshing
+showTask();
 
-var removeButton = document.getElementById("remove");
-removeButton.addEventListener("click", removeItem);
+let addTaskInput = document.getElementById("addtaskinput");
+let addTaskBtn = document.getElementById("addtaskbtn");
+// console.log(addTaskBtn);
 
-var ul = document.getElementById("list");
-var li;
+addTaskBtn.addEventListener("click", function () {
+  addTaskInputValue = addTaskInput.value;
 
-function addItem() {
-  var input = document.querySelector("#input");
-  var item = input.value;
+  //checking if the value is entered or not
+  if (addTaskInputValue.trim() != 0) {
+    let webTask = localStorage.getItem("localtask");
+    if (webTask == null) {
+      taskObj = [];
+    } else {
+      taskObj = JSON.parse(webTask);
+    }
 
-  ul = document.getElementById("list");
-  var textnode = document.createTextNode(item);
-
-  if (item === "") {
-    return false;
-    //A
-  } else {
-    //create li
-    li = document.createElement("li");
-
-    //create  a checkbox
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.setAttribute("id", "check");
-    //(id="check" )
-
-    //create label
-    var label = document.createElement("label");
-    label.setAttribute("for", "item"); //Optional
-
-    //Add elements on Webpage
-
-    ul.appendChild(label);
-    li.appendChild(checkbox);
-    label.appendChild(textnode);
-    li.appendChild(label);
-
-    ul.insertBefore(li, ul.childNodes[0]);
-
-    setTimeout(() => {
-      li.className = "visual";
-    }, 2);
-
-    input.value = "";
+    //adding values in array
+    taskObj.push({ task_name: addTaskInputValue, completeStatus: false });
+    //adding to local
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    addTaskInput.value = "";
   }
+  showTask();
+});
+
+function showTask() {
+  //getting items
+  let webTask = localStorage.getItem("localtask");
+  if (webTask == null) {
+    taskObj = [];
+  } else {
+    taskObj = JSON.parse(webTask);
+  }
+  let html = "";
+  let addedTaskList = document.getElementById("addedtasklist");
+
+  taskObj.forEach((item, index) => {
+    if (item.completeStatus == true) {
+      taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
+    } else {
+      taskCompleteValue = `<td>${item.task_name}</td>`;
+    }
+    html += `<tr>
+                    <th scope="row">${index + 1}</th>
+                    ${taskCompleteValue}
+                    <td><button type="button" onclick="edittask(${index})" class="text-primary"><i class="fa fa-edit"></i>Edit</button></td>
+                  
+                     <td><button type="button" onclick="deleteitem(${index})" class="text-danger"><i class="fa fa-trash"></i>Delete</button></td>
+               </tr>`;
+    //  <td><button type="button" class="text-success" id=${index}><i class="fa fa-check-square-o"></i>Complete</button></td>
+  });
+  addedTaskList.innerHTML = html;
 }
 
-function removeItem() {
-  li = ul.children;
-  // console.log(li);
-  for (let index = 0; index < li.length; index++) {
-    while (li[index] && li[index].children[0].checked) {
-      ul.removeChild(li[index]);
+// edittask
+function edittask(index) {
+  let saveIndex = document.getElementById("saveindex");
+  let addTaskBtn = document.getElementById("addtaskbtn");
+  let saveTaskBtn = document.getElementById("savetaskbtn");
+  saveIndex.value = index;
+
+  let webTask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webTask);
+  addTaskInput.value = taskObj[index]["task_name"];
+  addTaskBtn.style.display = "none";
+  saveTaskBtn.style.display = "block";
+}
+
+//savetask
+let saveTaskBtn = document.getElementById("savetaskbtn");
+saveTaskBtn.addEventListener("click", function () {
+  let webTask = localStorage.getItem("localtask");
+  let addTaskBtn = document.getElementById("addtaskbtn");
+  let taskObj = JSON.parse(webTask);
+  let saveIndex = document.getElementById("saveindex").value;
+
+  for (keys in taskObj[saveIndex]) {
+    if (keys == "task_name") {
+      taskObj[saveIndex].task_name = addTaskInput.value;
     }
   }
-  console.log("Delete Button Clicked");
+
+  // taskObj[saveIndex] = addTaskInput.value;
+  saveTaskBtn.style.display = "none";
+  addTaskBtn.style.display = "block";
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  addTaskInput.value = "";
+  showTask();
+});
+
+//delete
+function deleteitem(index) {
+  let webTask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webTask);
+
+  taskObj.splice(index, 1);
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  showTask();
 }
+
+//delete all
+let deleteAllBtn = document.getElementById("deleteallbtn");
+deleteAllBtn.addEventListener("click", function () {
+  let saveTaskBtn = document.getElementById("savetaskbtn");
+  let addTaskBtn = document.getElementById("addtaskbtn");
+
+  let webTask = localStorage.getItem("localtask");
+  let taskObj = JSON.parse(webTask);
+  if (webTask == null) {
+    taskObj = null;
+  } else {
+    taskObj = JSON.parse(webTask);
+    taskObj = [];
+  }
+
+  saveTaskBtn.style.display = "none";
+  addTaskBtn.style.display = "block";
+  localStorage.setItem("localtask", JSON.stringify(taskObj));
+  showTask();
+});
